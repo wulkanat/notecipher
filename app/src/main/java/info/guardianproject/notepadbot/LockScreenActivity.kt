50e48fd94c3825d893ffcd6bc4c0c6f6f6eca5fb
Wieland Schöbl <wulkanat@gmail.com>
2021-04-13 03:53:21 +0200

    Refactor LockscreenActivity to Kotlin

--- app/src/main/java/info/guardianproject/notepadbot/LockScreenActivity.kt
+++ app/src/main/java/info/guardianproject/notepadbot/LockScreenActivity.kt
@@ -1,273 +1,233 @@
+package info.guardianproject.notepadbot
 
-package info.guardianproject.notepadbot;
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Parcelable
+import android.os.ResultReceiver
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import android.widget.TextView.OnEditorActionListener
+import androidx.appcompat.app.AppCompatActivity
+import info.guardianproject.notepadbot.NConstants.validatePassword
+import info.guardianproject.notepadbot.cacheword.CacheWordActivityHandler
+import info.guardianproject.notepadbot.cacheword.ICacheWordSubscriber
+import java.security.GeneralSecurityException
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.ResultReceiver;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
-import android.widget.Toast;
-import android.widget.ViewFlipper;
+class LockScreenActivity : AppCompatActivity(), ICacheWordSubscriber {
+    private lateinit var mEnterPassphrase: EditText
+    private lateinit var mNewPassphrase: EditText
+    private lateinit var mConfirmNewPassphrase: EditText
+    private lateinit var mViewCreatePassphrase: View
+    private lateinit var mViewEnterPassphrase: View
+    private lateinit var mBtnOpen: Button
+    private lateinit var mCacheWord: CacheWordActivityHandler
+    private var mPasswordError: String? = null
+    private var mSlider: TwoViewSlider? = null
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import info.guardianproject.notepadbot.cacheword.CacheWordActivityHandler;
-import info.guardianproject.notepadbot.cacheword.ICacheWordSubscriber;
-
-import java.security.GeneralSecurityException;
-
-public class LockScreenActivity extends AppCompatActivity implements ICacheWordSubscriber {
-    private static final String TAG = "LockScreenActivity";
-
-    private EditText mEnterPassphrase;
-    private EditText mNewPassphrase;
-    private EditText mConfirmNewPassphrase;
-    private View mViewCreatePassphrase;
-    private View mViewEnterPassphrase;
-    private Button mBtnOpen;
-    private CacheWordActivityHandler mCacheWord;
-    private String mPasswordError;
-    private TwoViewSlider mSlider;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lock_screen);
-        mCacheWord = new CacheWordActivityHandler(this, ((App) getApplication()).getCWSettings());
-
-        mViewCreatePassphrase = findViewById(R.id.llCreatePassphrase);
-        mViewEnterPassphrase = findViewById(R.id.llEnterPassphrase);
-
-        mEnterPassphrase = (EditText) findViewById(R.id.editEnterPassphrase);
-        mNewPassphrase = (EditText) findViewById(R.id.editNewPassphrase);
-        mConfirmNewPassphrase = (EditText) findViewById(R.id.editConfirmNewPassphrase);
-        ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper1);
-        LinearLayout flipView1 = (LinearLayout) findViewById(R.id.flipView1);
-        LinearLayout flipView2 = (LinearLayout) findViewById(R.id.flipView2);
-
-        mSlider = new TwoViewSlider(vf, flipView1, flipView2, mNewPassphrase, mConfirmNewPassphrase);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lock_screen)
+        mCacheWord = CacheWordActivityHandler(this, (application as App).cWSettings)
+        mViewCreatePassphrase = findViewById(R.id.llCreatePassphrase)
+        mViewEnterPassphrase = findViewById(R.id.llEnterPassphrase)
+        mEnterPassphrase = findViewById<View>(R.id.editEnterPassphrase) as EditText
+        mNewPassphrase = findViewById<View>(R.id.editNewPassphrase) as EditText
+        mConfirmNewPassphrase = findViewById<View>(R.id.editConfirmNewPassphrase) as EditText
+        val vf = findViewById<View>(R.id.viewFlipper1) as ViewFlipper
+        val flipView1 = findViewById<View>(R.id.flipView1) as LinearLayout
+        val flipView2 = findViewById<View>(R.id.flipView2) as LinearLayout
+        mSlider = TwoViewSlider(vf, flipView1, flipView2, mNewPassphrase, mConfirmNewPassphrase)
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mCacheWord.onPause();
+    override fun onPause() {
+        super.onPause()
+        mCacheWord.onPause()
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mCacheWord.onResume();
+    override fun onResume() {
+        super.onResume()
+        mCacheWord.onResume()
     }
 
-    @Override
-    public void onCacheWordUninitialized() {
-        initializePassphrase();
+    override fun onCacheWordUninitialized() {
+        initializePassphrase()
     }
 
-    @Override
-    public void onCacheWordLocked() {
-        promptPassphrase();
+    override fun onCacheWordLocked() {
+        promptPassphrase()
     }
 
-    @Override
-    public void onCacheWordOpened() {
-        Intent intent = (Intent) getIntent().getParcelableExtra("originalIntent");
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        startActivity(intent);
-        finish();
-        overridePendingTransition(0, 0);
+    override fun onCacheWordOpened() {
+        val intent = intent.getParcelableExtra<Parcelable>("originalIntent") as Intent?
+        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+        finish()
+        overridePendingTransition(0, 0)
     }
 
-    private boolean newEqualsConfirmation() {
-        return !mNewPassphrase.getText().toString()
-                .equals(mConfirmNewPassphrase.getText().toString());
+    private fun newEqualsConfirmation(): Boolean {
+        return mNewPassphrase.text.toString() != mConfirmNewPassphrase.text.toString()
     }
 
-    private void showValidationError() {
-        Toast.makeText(LockScreenActivity.this, mPasswordError, Toast.LENGTH_LONG).show();
-        mNewPassphrase.requestFocus();
+    private fun showValidationError() {
+        Toast.makeText(this@LockScreenActivity, mPasswordError, Toast.LENGTH_LONG).show()
+        mNewPassphrase.requestFocus()
     }
 
-    private void showInequalityError() {
-        Toast.makeText(LockScreenActivity.this,
-                R.string.lock_screen_passphrases_not_matching,
-                Toast.LENGTH_SHORT).show();
-        clearNewFields();
+    private fun showInequalityError() {
+        Toast.makeText(
+            this@LockScreenActivity,
+            R.string.lock_screen_passphrases_not_matching,
+            Toast.LENGTH_SHORT
+        ).show()
+        clearNewFields()
     }
 
-    private void clearNewFields() {
-        mNewPassphrase.getEditableText().clear();
-        mConfirmNewPassphrase.getEditableText().clear();
+    private fun clearNewFields() {
+        mNewPassphrase.editableText.clear()
+        mConfirmNewPassphrase.editableText.clear()
     }
 
-    private boolean isPasswordNotValid() {
-        boolean valid = NConstants.validatePassword(mNewPassphrase.getText().toString().toCharArray());
-        if (!valid)
-            mPasswordError = getString(R.string.pass_err_length);
-        return !valid;
-    }
+    private val isPasswordNotValid: Boolean
+        get() {
+            val valid = validatePassword(mNewPassphrase.text.toString().toCharArray())
+            if (!valid) mPasswordError = getString(R.string.pass_err_length)
+            return !valid
+        }
+    private val isConfirmationFieldEmpty: Boolean
+        get() = mConfirmNewPassphrase.text.toString().isEmpty()
 
-    private boolean isConfirmationFieldEmpty() {
-        return mConfirmNewPassphrase.getText().toString().length() == 0;
-    }
-
-    private void initializePassphrase() {
+    private fun initializePassphrase() {
         // Passphrase is not set, so allow the user to create one
-
-        mViewCreatePassphrase.setVisibility(View.VISIBLE);
-        mViewEnterPassphrase.setVisibility(View.GONE);
-
-        mNewPassphrase.setOnEditorActionListener((v, actionId, event) -> {
+        mViewCreatePassphrase.visibility = View.VISIBLE
+        mViewEnterPassphrase.visibility = View.GONE
+        mNewPassphrase.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_NULL || actionId == EditorInfo.IME_ACTION_DONE) {
-                if (isPasswordNotValid())
-                    showValidationError();
-                else
-                    mSlider.showConfirmationField();
+                if (isPasswordNotValid) showValidationError() else mSlider!!.showConfirmationField()
             }
-            return false;
-        });
-
-        mConfirmNewPassphrase.setOnEditorActionListener((v, actionId, event) -> {
+            false
+        }
+        mConfirmNewPassphrase.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_NULL || actionId == EditorInfo.IME_ACTION_DONE) {
                 if (newEqualsConfirmation()) {
-                    showInequalityError();
-                    mSlider.showNewPasswordField();
+                    showInequalityError()
+                    mSlider!!.showNewPasswordField()
                 }
             }
-            return false;
-        });
-
-        Button btnCreate = (Button) findViewById(R.id.btnCreate);
-        btnCreate.setOnClickListener(v -> {
+            false
+        }
+        val btnCreate = findViewById<View>(R.id.btnCreate) as Button
+        btnCreate.setOnClickListener {
             // validate
-            if (isPasswordNotValid()) {
-                showValidationError();
-                mSlider.showNewPasswordField();
-            } else if (isConfirmationFieldEmpty()) {
-                mSlider.showConfirmationField();
-            } else if (newEqualsConfirmation()) {
-                showInequalityError();
-                mSlider.showNewPasswordField();
-            } else {
-                try {
-                    mCacheWord.setPassphrase(mNewPassphrase.getText().toString().toCharArray());
-                } catch (GeneralSecurityException e) {
-                    // TODO initialization failed
-                    Log.e(TAG, "Cache word pass initialization failed: " + e.getMessage());
+            when {
+                isPasswordNotValid -> {
+                    showValidationError()
+                    mSlider!!.showNewPasswordField()
+                }
+                isConfirmationFieldEmpty -> {
+                    mSlider!!.showConfirmationField()
+                }
+                newEqualsConfirmation() -> {
+                    showInequalityError()
+                    mSlider!!.showNewPasswordField()
+                }
+                else -> {
+                    try {
+                        mCacheWord.setPassphrase(mNewPassphrase.text.toString().toCharArray())
+                    } catch (e: GeneralSecurityException) {
+                        // TODO initialization failed
+                        Log.e(TAG, "Cache word pass initialization failed: " + e.message)
+                    }
                 }
             }
-        });
+        }
     }
 
-    private void promptPassphrase() {
-        mViewCreatePassphrase.setVisibility(View.GONE);
-        mViewEnterPassphrase.setVisibility(View.VISIBLE);
-
-        mBtnOpen = (Button) findViewById(R.id.btnOpen);
-        mBtnOpen.setOnClickListener(v -> {
-            if (mEnterPassphrase.getText().toString().length() == 0)
-                return;
+    private fun promptPassphrase() {
+        mViewCreatePassphrase.visibility = View.GONE
+        mViewEnterPassphrase.visibility = View.VISIBLE
+        mBtnOpen = findViewById<View>(R.id.btnOpen) as Button
+        mBtnOpen.setOnClickListener {
+            if (mEnterPassphrase.text.toString().isEmpty()) return@setOnClickListener
             // Check passphrase
             try {
-                mCacheWord.setPassphrase(mEnterPassphrase.getText().toString().toCharArray());
-            } catch (GeneralSecurityException e) {
-                mEnterPassphrase.setText("");
+                mCacheWord.setPassphrase(mEnterPassphrase.text.toString().toCharArray())
+            } catch (e: GeneralSecurityException) {
+                mEnterPassphrase.setText("")
                 // TODO implement try again and wipe if fail
-                Log.e(TAG, "Cache word pass verification failed: " + e.getMessage());
+                Log.e(TAG, "Cache word pass verification failed: " + e.message)
             }
-        });
-
-        mEnterPassphrase.setOnEditorActionListener(new OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_NULL || actionId == EditorInfo.IME_ACTION_GO) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                    Handler threadHandler = new Handler();
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0, new ResultReceiver(
-                            threadHandler) {
-                        @Override
-                        protected void onReceiveResult(int resultCode, Bundle resultData) {
-                            super.onReceiveResult(resultCode, resultData);
-                            mBtnOpen.performClick();
-                        }
-                    });
-                    return true;
-                }
-                return false;
+        }
+        mEnterPassphrase.setOnEditorActionListener(OnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_NULL || actionId == EditorInfo.IME_ACTION_GO) {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                val threadHandler = Handler()
+                imm.hideSoftInputFromWindow(v.windowToken, 0, object : ResultReceiver(
+                    threadHandler
+                ) {
+                    override fun onReceiveResult(resultCode: Int, resultData: Bundle) {
+                        super.onReceiveResult(resultCode, resultData)
+                        mBtnOpen.performClick()
+                    }
+                })
+                return@OnEditorActionListener true
             }
-        });
+            false
+        })
     }
 
-    public class TwoViewSlider {
-
-        private boolean firstIsShown = true;
-        private final ViewFlipper flipper;
-        private final LinearLayout container1;
-        private final LinearLayout container2;
-        private final View firstView;
-        private final View secondView;
-        private final Animation pushRightIn;
-        private final Animation pushRightOut;
-        private final Animation pushLeftIn;
-        private final Animation pushLeftOut;
-
-        public TwoViewSlider(ViewFlipper flipper, LinearLayout container1, LinearLayout container2,
-                             View view1, View view2) {
-            this.flipper = flipper;
-            this.container1 = container1;
-            this.container2 = container2;
-            this.firstView = view1;
-            this.secondView = view2;
-
-            pushRightIn = AnimationUtils.loadAnimation(LockScreenActivity.this, R.anim.push_right_in);
-            pushRightOut = AnimationUtils.loadAnimation(LockScreenActivity.this, R.anim.push_right_out);
-            pushLeftIn = AnimationUtils.loadAnimation(LockScreenActivity.this, R.anim.push_left_in);
-            pushLeftOut = AnimationUtils.loadAnimation(LockScreenActivity.this, R.anim.push_left_out);
-
+    inner class TwoViewSlider(
+        private val flipper: ViewFlipper,
+        private val container1: LinearLayout,
+        private val container2: LinearLayout,
+        private val firstView: View?,
+        private val secondView: View?
+    ) {
+        private var firstIsShown = true
+        private val pushRightIn: Animation =
+            AnimationUtils.loadAnimation(this@LockScreenActivity, R.anim.push_right_in)
+        private val pushRightOut: Animation =
+            AnimationUtils.loadAnimation(this@LockScreenActivity, R.anim.push_right_out)
+        private val pushLeftIn: Animation =
+            AnimationUtils.loadAnimation(this@LockScreenActivity, R.anim.push_left_in)
+        private val pushLeftOut: Animation =
+            AnimationUtils.loadAnimation(this@LockScreenActivity, R.anim.push_left_out)
+        fun showNewPasswordField() {
+            if (firstIsShown) return
+            flipper.inAnimation = pushRightIn
+            flipper.outAnimation = pushRightOut
+            flip()
         }
 
-        public void showNewPasswordField() {
-            if (firstIsShown)
-                return;
-
-            flipper.setInAnimation(pushRightIn);
-            flipper.setOutAnimation(pushRightOut);
-            flip();
+        fun showConfirmationField() {
+            if (!firstIsShown) return
+            flipper.inAnimation = pushLeftIn
+            flipper.outAnimation = pushLeftOut
+            flip()
         }
 
-        public void showConfirmationField() {
-            if (!firstIsShown)
-                return;
-
-            flipper.setInAnimation(pushLeftIn);
-            flipper.setOutAnimation(pushLeftOut);
-            flip();
-        }
-
-        private void flip() {
+        private fun flip() {
             if (firstIsShown) {
-                firstIsShown = false;
-                container2.removeAllViews();
-                container2.addView(secondView);
+                firstIsShown = false
+                container2.removeAllViews()
+                container2.addView(secondView)
             } else {
-                firstIsShown = true;
-                container1.removeAllViews();
-                container1.addView(firstView);
+                firstIsShown = true
+                container1.removeAllViews()
+                container1.addView(firstView)
             }
-            flipper.showNext();
+            flipper.showNext()
         }
+
+    }
+
+    companion object {
+        private const val TAG = "LockScreenActivity"
     }
 }
