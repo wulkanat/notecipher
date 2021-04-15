40f22deae94998a66baa3f0b48e78d83df23f5f2
Wieland Schöbl <wulkanat@gmail.com>
2021-04-15 16:32:46 +0200

    Card theme

--- app/src/main/java/info/guardianproject/notepadbot/NotesAdapter.kt
+++ app/src/main/java/info/guardianproject/notepadbot/NotesAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import info.guardianproject.notepadbot.databinding.NoteSmallFragmentBinding
 
-class NotesAdapter(private val dataSet: Array<String>) :
+class NotesAdapter(private val dataSet: Array<Pair<String, String>>) :
     RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: NoteSmallFragmentBinding) :
@@ -21,7 +21,9 @@ class NotesAdapter(private val dataSet: Array<String>) :
         )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.titleText.text = dataSet[position]
+        val (title, data) = dataSet[position]
+        holder.binding.titleText.text = title
+        holder.binding.supportText.text = data
     }
 
     override fun getItemCount() = dataSet.size
--- app/src/main/java/info/guardianproject/notepadbot/fragments/NotesFragment.kt
+++ app/src/main/java/info/guardianproject/notepadbot/fragments/NotesFragment.kt
@@ -1,24 +1,37 @@
 package info.guardianproject.notepadbot.fragments
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.navigation.NavOptions
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import info.guardianproject.notepadbot.NotesAdapter
 import info.guardianproject.notepadbot.R
-import info.guardianproject.notepadbot.databinding.LockscreenFragmentBinding
 import info.guardianproject.notepadbot.databinding.NotesFragmentBinding
 import kotlin.math.roundToInt
+import kotlin.random.Random
 
 class NotesFragment : Fragment(R.layout.notes_fragment) {
     private lateinit var _binding: NotesFragmentBinding
     private val binding get() = _binding
+
+    /**
+     * TODO: remove
+     */
+    private fun random(): String {
+        val randomStringBuilder = StringBuilder()
+        val randomLength: Int = Random.nextInt(500)
+        var tempChar: Char
+        for (i in 0 until randomLength) {
+            tempChar = ((Random.nextInt(96) + 32).toChar())
+            randomStringBuilder.append(tempChar)
+        }
+        return randomStringBuilder.toString()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +41,7 @@ class NotesFragment : Fragment(R.layout.notes_fragment) {
         _binding = it
         it.notesList.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         it.notesList.adapter = NotesAdapter(
-            Array(40) { i -> "Test$i" }
+            Array(40) { i -> "Test$i" to random() }
         )
     }.root.also {
         ViewCompat.setOnApplyWindowInsetsListener(binding.notesList) { v, insets ->
