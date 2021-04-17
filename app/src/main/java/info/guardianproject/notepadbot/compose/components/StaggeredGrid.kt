3a221807d5ea9bb9d508e9bc0ffe3e1dcd63373b
Wieland Schöbl <wulkanat@gmail.com>
2021-04-17 23:50:00 +0200

    Add more compose stuff

---
+++ app/src/main/java/info/guardianproject/notepadbot/compose/components/StaggeredGrid.kt
@@ -0,0 +1,27 @@
+package info.guardianproject.notepadbot.compose.components
+
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+
+/*@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 2,
+    content: @Composable () -> Unit,
+) {
+    LazyVerticalGrid(cells = ) {
+        
+    }
+    
+    Layout(
+        modifier = modifier,
+        content = content
+    ) {
+        val rowWidths = IntArray(rows) { 0 }
+        val rowMaxHeights = IntArray(rows) { 0 }
+        
+        this.layout()
+    }
+}*/
